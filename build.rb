b73574e3cfd037b588e9d559cf8477eb0da02dcb
suzusime <tamagawajousuiro+git@gmail.com>
2020-12-05 23:09:14 +0900

    pandocでのビルドが最小限できるようになった

---
+++ build.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+# pandocのオプションを設定
+PANDOC='pandoc -f markdown+east_asian_line_breaks -t latex -N --pdf-engine=lualatex --filter pandoc-citeproc --top-level-division=chapter'
+
+# 記事を順番に書く
+FILELIST=<<'EOS'
+top.md
+recent-kmc.md
+recent-kmc/kyopro.md
+EOS
+
+# MarkdownをTeXに変換
+files = FILELIST.gsub(/^/, "kiji/").gsub(/\n/, " ")
+system("#{PANDOC} -o out/hoge.pdf #{files}")
+
---
+++ kiji/top.md
@@ -0,0 +1,11 @@
+---
+title: "独習KMC vol. 16"
+#author: "KMC"
+documentclass: "bxjsbook"
+papersize: b5
+classoption: ["pandoc", "jbase=12Q", "magstyle=nomag*", "jafont=ipaex"]
+# bibliography: "main.bib"
+# csl: "physical-review-a.csl"
+# header-includes: |
+#   
+---
