2113fdb5f25848ae603b5980d1a05bbe9d518e10
suzusime <tamagawajousuiro+git@gmail.com>
2020-12-05 23:25:28 +0900

    校正規約を雑に追加

--- build.rb
+++ build.rb
@@ -1,13 +1,15 @@
 #!/usr/bin/env ruby
 
 # pandocのオプションを設定
-PANDOC='pandoc -f markdown+east_asian_line_breaks -t latex -N --pdf-engine=lualatex --filter pandoc-citeproc --top-level-division=chapter'
+PANDOC='pandoc -f markdown+east_asian_line_breaks -t latex -N --pdf-engine=lualatex --filter pandoc-citeproc --top-level-division=chapter --table-of-contents --toc-depth=3'
 
 # 記事を順番に書く
 FILELIST=<<'EOS'
 top.md
 recent-kmc.md
 recent-kmc/kyopro.md
+columns.md
+kousei.md
 EOS
 
 # MarkdownをTeXに変換
