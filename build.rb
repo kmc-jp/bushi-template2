b285d442e9262ea89c3232cb39feb9ebeafb295f
suzusime <tamagawajousuiro+git@gmail.com>
2020-12-06 00:28:53 +0900

    各ファイル別々にTeXに変換するようにした

--- build.rb
+++ build.rb
@@ -1,18 +1,19 @@
 #!/usr/bin/env ruby
-
+require 'fileutils'
 # pandocのオプションを設定
 PANDOC='pandoc -f markdown+east_asian_line_breaks -t latex -N --pdf-engine=lualatex --filter pandoc-citeproc --top-level-division=part --table-of-contents --toc-depth=3'
 
-# 記事を順番に書く
-FILELIST=<<'EOS'
-top.md
-recent-kmc.md
-recent-kmc/kyopro.md
-columns.md
-kousei.md
-EOS
-
 # MarkdownをTeXに変換
-files = FILELIST.gsub(/^/, "kiji/").gsub(/\n/, " ")
-system("#{PANDOC} -o out/hoge.pdf #{files}")
+Dir.glob('kiji/**/*') do |file|
+  if FileTest.file? file then
+    outfile = file.sub(/^kiji\//, '').sub(/\//, '-').sub(/.md$/, '.tex')
+    system("#{PANDOC} -o out/#{outfile} #{file}")
+  end
+end
+
+# 全体をPDFに変換
+FileUtils.cp("bushi.tex", "out/bushi.tex")
+FileUtils.cd("out") do
+  system("latexmk -lualatex bushi.tex")
+end
 
