adaebdfd0039c7bc6385e72ed0f0141f37481029
suzusime <tamagawajousuiro+git@gmail.com>
2020-12-05 23:33:20 +0900

    最大要素をPartに変更

--- build.rb
+++ build.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 # pandocのオプションを設定
-PANDOC='pandoc -f markdown+east_asian_line_breaks -t latex -N --pdf-engine=lualatex --filter pandoc-citeproc --top-level-division=chapter --table-of-contents --toc-depth=3'
+PANDOC='pandoc -f markdown+east_asian_line_breaks -t latex -N --pdf-engine=lualatex --filter pandoc-citeproc --top-level-division=part --table-of-contents --toc-depth=3'
 
 # 記事を順番に書く
 FILELIST=<<'EOS'
